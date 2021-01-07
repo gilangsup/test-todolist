3524cfe3c1519dba6fd066912c0201eba382d74e
gilangsup <gilangsprb@gmail.com>
2021-01-07 21:26:53 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 // import SearchPage from './components/SearchPage';
-import {Container} from 'reactstrap'
-import axios from 'axios'
+import { Container } from "reactstrap";
+import axios from "axios";
 
 function App() {
-
-  const [search, setSearch] = useState('')
-  const [project, setProject] = useState()
-  const axios = require('axios')
+  const [search, setSearch] = useState("");
+  const [project, setProject] = useState();
+  const axios = require("axios");
   const getData = () => {
-    axios.get('http://demo7617443.mockable.io/getProjects').then(res => {
-      setProject(res.data)
-    })
-  }
-  useEffect( () => {getData()},[]);
+    axios.get("http://demo7617443.mockable.io/getProjects").then((res) => {
+      setProject(res.data);
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="App">
-      
       <input
         type="text"
         placeholder="Search Task or Project...."
-        onChange={(e) => {setSearch(e.target.value)}}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
       />
-      {getData.filter((val) => {
-        if (search == '') {
-          return val
-        } else if (val.projects.toLowerCase().includes(search.toLowerCase())){
-          return val
-        }
-      }).map((val,key) => {
-        return (
-         <div key={key}>
-           <p>{val.projects}</p>
-         </div>
-        )
-      })}
+      {project
+        .filter((val) => {
+          if (search == "") {
+            return val;
+          } else if (
+            val.projects.toLowerCase().includes(search.toLowerCase())
+          ) {
+            return val;
+          }
+        })
+        .map((val, key) => {
+          return (
+            <div key={key}>
+              <p>{val.projects}</p>
+            </div>
+          );
+        })}
       {/* <SearchPage/> */}
     </div>
   );
